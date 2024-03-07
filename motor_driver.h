0fc8cacfefb17969a6fefc1f43f0173aa72212d0
mohitgpt07 <109983686+mohitgpt07@users.noreply.github.com>
2024-03-07 16:41:05 +0530

    Add files via upload

---
+++ motor_driver.h
@@ -0,0 +1,17 @@
+/***************************************************************
+   Motor driver function definitions - by Mohit Kumar Gupta
+   *************************************************************/
+
+#ifdef L298_MOTOR_DRIVER
+  #define LEFT_MOTOR_ENABLE 5
+  #define LEFT_MOTOR_FORWARD 7
+  #define LEFT_MOTOR_BACKWARD 8
+  #define RIGHT_MOTOR_FORWARD 11
+  #define RIGHT_MOTOR_BACKWARD 9
+  #define RIGHT_MOTOR_ENABLE 6
+  
+#endif
+
+void initMotorController();
+void setMotorSpeed(int i, int spd);
+void setMotorSpeeds(int leftSpeed, int rightSpeed);
