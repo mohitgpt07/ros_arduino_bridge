66458d8a7f2978a633439fcd16f4e92802f06e89
mohitgpt07 <mohitgpt0823@gmail.com>
2024-04-21 16:24:33 +0530

    Correct the pin configuration

--- motor_driver.h
+++ motor_driver.h
@@ -4,12 +4,12 @@
 
 #ifdef L298_MOTOR_DRIVER
   //below can be changed
-  #define LEFT_MOTOR_ENABLE 5
-  #define LEFT_MOTOR_FORWARD 7
-  #define LEFT_MOTOR_BACKWARD 8
-  #define RIGHT_MOTOR_FORWARD 11
+  #define LEFT_MOTOR_ENABLE 7
+  #define LEFT_MOTOR_FORWARD 6
+  #define LEFT_MOTOR_BACKWARD 11
+  #define RIGHT_MOTOR_FORWARD 5
   #define RIGHT_MOTOR_BACKWARD 9
-  #define RIGHT_MOTOR_ENABLE 6
+  #define RIGHT_MOTOR_ENABLE 8
   
 #endif
 
